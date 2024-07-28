80ff0783ed7da5653987b7fa8ea370734ca82832
takao77 <157527196+takao77@users.noreply.github.com>
2024-07-28 13:43:42 -0400

    Add files via upload

--- startup.sh
+++ startup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Install ffmpeg
-apt-get update
-apt-get install -y ffmpeg
+apt-get update && apt-get install -y ffmpeg
 
 # Start the Gunicorn server
 gunicorn --bind=0.0.0.0:8000 app_s:app
